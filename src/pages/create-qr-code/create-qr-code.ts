b37073dda2033309629c5732d5814fea7713ac03
KevSib <ksibue@gmail.com>
2019-03-01 12:25:40 +0100

    import lots of library and update qrcode provider

--- src/pages/create-qr-code/create-qr-code.ts
+++ src/pages/create-qr-code/create-qr-code.ts
@@ -1,6 +1,8 @@
+import { QrCodeProvider } from './../../providers/qr-code/qr-code';
 import { BasePage } from './../base-page.page';
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { QRCodeComponent } from 'angularx-qrcode';
 
 /**
  * Generated class for the CreateQrCodePage page.
@@ -19,17 +21,25 @@ export class CreateQrCodePage extends BasePage {
     qrCodeInput: string = "";
     qrCodeGenerated: boolean = false;
 
-    constructor(public navCtrl: NavController, public navParams: NavParams) {
+    @ViewChild(QRCodeComponent) 
+    private qrCodeRender: QRCodeComponent;
+
+    constructor(public navCtrl: NavController, 
+                public navParams: NavParams,
+                private qrCodeProvider: QrCodeProvider) {
         super();
         this.pageTitle = "Génération d'un QRCode";
     }
 
-    generate(): void {
-
+    share(): void {
+        const svgData = this.qrCodeRender.el.nativeElement.children[0].outerHTML;
+        const preface = '<?xml version="1.0" standalone="no"?>\r\n';
+        const svgBlob = new Blob([preface, svgData], {type:"image/svg+xml;charset=utf-8"});
+        this.qrCodeProvider.shareQRCode(svgBlob);
     }
 
-    share(): void {
-
+    canShowQRCode(): boolean {
+        return this.qrCodeInput !== '' && this.qrCodeInput !== undefined;
     }
 
 }
--- src/providers/qr-code/qr-code.ts
+++ src/providers/qr-code/qr-code.ts
@@ -1,5 +1,9 @@
 import { HttpClient } from '@angular/common/http';
+import { FilePath } from '@ionic-native/file-path';
 import { Injectable } from '@angular/core';
+import { SocialSharing } from '@ionic-native/social-sharing';
+import { AndroidPermissions } from '@ionic-native/android-permissions';
+import { File } from '@ionic-native/file';
 
 /*
   Generated class for the QrCodeProvider provider.
@@ -10,13 +14,89 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class QrCodeProvider {
 
-    constructor(public http: HttpClient) {
+    static DEFAULT_TEMP_FILENAME: string = 'qrcode.svg';
+
+    constructor(public http: HttpClient,
+                private filePath: FilePath,
+                private socialSharing: SocialSharing,
+                private androidPermissions: AndroidPermissions,
+                private file: File) {
         
     }
 
     generate(text: string): Promise<string> {
-        //TODO: Code this
-        return Promise.resolve('');
+        return Promise.resolve(text);
+    }
+
+    shareQRCode(blob: Blob): Promise<boolean> {
+        return new Promise((resolve, reject) => {
+            this.getWritePermission()
+                .then((permissionResult) => {
+                    if (permissionResult) {
+                        this.createTmpFile(blob, QrCodeProvider.DEFAULT_TEMP_FILENAME)
+                            .then((res) => {
+                                console.log('res', res)
+                                this.socialSharing.share('', '', res);
+                            })
+                            .catch((error) => {
+                                console.log('err', error)
+                            });
+                    } else {
+                        resolve(false);
+                    }
+                });
+        });
+    }
+
+    getWritePermission(): Promise<boolean> {
+        return new Promise((resolve, reject) => {
+            this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.WRITE_EXTERNAL_STORAGE)
+                .then((result) => {
+                    if (result.hasPermission === true) {
+                        resolve(true);
+                    } else {
+                        throw new Error('No permission');
+                    }
+                })
+                .catch((err) => {
+                    this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.WRITE_EXTERNAL_STORAGE)
+                        .then((res) => {
+                            resolve(true);
+                        })
+                        .catch((errr) => {
+                            resolve(false);
+                        });
+                });
+        });
+    }
+
+    createTmpFile(content: string | Blob, filename: string): Promise<any> {
+        return new Promise((resolve, reject) => {
+            this.file.createFile(this.getDataDirectory(), filename, true)
+                .then(() => {
+                    this.file.writeExistingFile(this.getDataDirectory(), filename, content)
+                        .then(async (res) => {
+                            const path = await this.resolvePath(this.getDataDirectory() + '/' + filename);
+                            resolve(path);
+                        })
+                        .catch((err) => {
+                            console.log('writeExistingFile error', err);
+                            reject();
+                        });
+                })
+                .catch((err) => {
+                    console.log('createFile error', err);
+                    reject();
+                });
+        });
+    }
+
+    getDataDirectory(): string {
+        return this.file.cacheDirectory;
+    }
+
+    resolvePath(path: string): Promise<string> {
+        return this.filePath.resolveNativePath(path);
     }
 
 }
