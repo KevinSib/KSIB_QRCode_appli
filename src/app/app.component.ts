6d6b33c91454e0122e06dc17651177c7fe10bb8e
KevSib <ksibue@gmail.com>
2019-03-01 09:31:37 +0100

    update app.component to manage new pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,46 @@
+import { HistoricPage } from './../pages/historic/historic';
+import { ReadQrCodePage } from './../pages/read-qr-code/read-qr-code';
+import { CreateQrCodePage } from './../pages/create-qr-code/create-qr-code';
 import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
-
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  @ViewChild(Nav) nav: Nav;
+  
+    @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = HomePage;
+    rootPage: any = CreateQrCodePage;
 
-  pages: Array<{title: string, component: any}>;
+    pages: Array<{title: string, component: any}>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
-    this.initializeApp();
+    constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+        this.initializeApp();
 
-    // used for an example of ngFor and navigation
-    this.pages = [
-      { title: 'Génération de QRCode', component: HomePage },
-      { title: 'Lecture de QRCode', component: ListPage },
-      { title: 'Historique', component: ListPage },
-    ];
+        // used for an example of ngFor and navigation
+        this.pages = [
+        { title: 'Génération de QRCode', component: CreateQrCodePage },
+        { title: 'Lecture de QRCode', component: ReadQrCodePage },
+        { title: 'Historique', component: HistoricPage },
+        ];
 
-  }
+    }
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
-  }
+    initializeApp() {
+        this.platform.ready().then(() => {
+        // Okay, so the platform is ready and our plugins are available.
+        // Here you can do any higher level native things you might need.
+        this.statusBar.styleDefault();
+        this.splashScreen.hide();
+        });
+    }
 
-  openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
-  }
+    openPage(page) {
+        // Reset the content nav to have just this page
+        // we wouldn't want the back button to show in this scenario
+        this.nav.setRoot(page.component);
+    }
 }
